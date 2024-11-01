342067cc7a66077aaa09cdc3da7822554adac41c
CodeGen2309 <gosha31rus.14@gmail.com>
2024-11-01 19:33:08 +0300

    liitle reform

--- installs/qtile.sh
+++ installs/qtile.sh
@@ -1,7 +1,46 @@
 #! /bin/bash
 
-
+# SINGLE APPS
 packgs="
+webapp-manager
+code
+obsidian
+vivaldi
+flatpak
+qbittorrent
+python-pillow
+zoom
+git
+otf-font-awesome
+"
+
+
+#NODE JS
+packgs+="
+nodejs
+npm
+"
+
+#DOCKER
+packgs+="
+docker
+docker-compose
+"
+
+
+#CONSOLE UTILS
+packgs+="
+kitty
+ranger
+micro
+atool
+btop
+"
+
+
+
+#QTILE AND APPEARANCE 
+packgs+="
 rofi
 qtile
 picom
@@ -9,4 +48,16 @@ nitrogen
 "
 
 
-sudo pamac install $packgs
+# FATPACK APPS
+flatlist="
+org.onlyoffice.desktopeditors
+com.anydesk.Anydesk
+com.getpostman.Postman
+com.google.Chrome
+ru.yandex.Browser
+"
+
+
+
+pamac install $packgs
+flatpak install $flatlist
