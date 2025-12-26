502f01a05ebeaf1364df88c80db50939d80f6c0a
CodeGen2309 <gosha31rus.14@gmail.com>
2025-12-26 16:51:47 +0300

    lil updates

---
+++ packages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+systemPackages="
+  fastfetch pipewire  playerctl  networkmanager  brightnessctl  pkgconf
+  wf-recorder nemo gvfs  alacritty libnotify python gtk3 cairo 
+  gtk-layer-shell libgirepository gobject-introspection gobject-introspection-runtime 
+  python-pip python-gobject python-psutil python-cairo python-dbus 
+  python-pydbus python-loguru python-setproctitle grim swappy code
+"
+
+
+cliPackages=" 
+  btop zsh ranger screen git micro podman nodejs npm
+"
+
+hyprPackages="
+  hyprland hyprpaper hyprshot hyprlauncher hypridle hyprlock 
+  xdg-desktop-portal-hyprland hyprsunset hyprpolkitagent
+  hyprland-qt-support hyprqt6engine hyprpwcenter hyprcursor
+  aquamarine waybar
+"
+
+utils="
+  mako rofi
+"
