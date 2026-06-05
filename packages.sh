11f3f8b55655198d0bc43ffb30e7a42ed46eac36
CodeGen2309 <gosha31rus.14@gmail.com>
2026-06-05 15:51:26 +0300

    update install packages

--- packages.sh
+++ packages.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
 
-systemPackages="
-  fastfetch pipewire  playerctl  networkmanager  brightnessctl  pkgconf
-  wf-recorder nemo gvfs  alacritty libnotify python gtk3 cairo 
-  gtk-layer-shell libgirepository gobject-introspection gobject-introspection-runtime 
-  python-pip python-gobject python-psutil python-cairo python-dbus 
-  python-pydbus python-loguru python-setproctitle grim swappy code
-"
-
-
 cliPackages=" 
-  btop zsh ranger screen git micro podman nodejs npm
+  btop zsh ranger screen git micro podman python nodejs npm yay
 "
 
-hyprPackages="
-  hyprland hyprpaper hyprshot hyprlauncher hypridle hyprlock 
-  xdg-desktop-portal-hyprland hyprsunset hyprpolkitagent
-  hyprland-qt-support hyprqt6engine hyprpwcenter hyprcursor
-  aquamarine waybar
+
+niriPackages="
+  niri xwayland-satellite xdg-desktop-portal-gnome 
+  xdg-desktop-portal-gtk alacritty cava qt6-multimedia-ffmpeg
 "
+
 
 utils="
-  mako rofi
+  mako rofi nemo fastfetch
 "
